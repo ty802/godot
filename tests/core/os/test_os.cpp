78f3fef69b39f3e0b7e119211f5059e7b2138055
ty802 <31832680+ty802@users.noreply.github.com>
2026-06-09 20:57:38 -0500

    remove unclosed "(" in error messages.

--- tests/core/os/test_os.cpp
+++ tests/core/os/test_os.cpp
@@ -181,7 +181,7 @@ TEST_CASE("[OS] Execute") {
 	const Error err = OS::get_singleton()->execute("cmd", arguments, nullptr, &exit_code);
 	CHECK_MESSAGE(
 			err == OK,
-			"(Running the command `cmd /C \"dir > NUL\"` returns the expected Godot error code (OK).");
+			"Running the command `cmd /C \"dir > NUL\"` returns the expected Godot error code (OK).");
 	CHECK_MESSAGE(
 			exit_code == 0,
 			"Running the command `cmd /C \"dir > NUL\"` returns a zero (successful) exit code.");
@@ -193,7 +193,7 @@ TEST_CASE("[OS] Execute") {
 	const Error err = OS::get_singleton()->execute("sh", arguments, nullptr, &exit_code);
 	CHECK_MESSAGE(
 			err == OK,
-			"(Running the command `sh -c \"ls > /dev/null\"` returns the expected Godot error code (OK).");
+			"Running the command `sh -c \"ls > /dev/null\"` returns the expected Godot error code (OK).");
 	CHECK_MESSAGE(
 			exit_code == 0,
 			"Running the command `sh -c \"ls > /dev/null\"` returns a zero (successful) exit code.");
